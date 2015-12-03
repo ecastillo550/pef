260f837a5c47691c76c5f3d1e7e8301dbd1ebb63
Erick Castillo de la Garza <ecastillo550@gmail.com>
2015-12-03 11:18:30 -0600

    se establecio una direccion relativa

--- public_html/pef/index.php
+++ public_html/pef/index.php
@@ -3,8 +3,8 @@ error_reporting(E_ALL | E_STRICT);
 ini_set('display_errors', 1);
 //arreglo designado para hacer convivir a mas apps de Hagane <3
 $HaganeInit = array(
-	'appFolderName' => 'app',
-	'appFolderDepth' => '../'
+	'appFolderName' => 'app-pef',
+	'appFolderDepth' => '../../'
 );
 
 include_once($HaganeInit['appFolderDepth'].'lib/init.php');
