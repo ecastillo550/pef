cf14d192beed717c707a8d6572d4aad5249f34b7
Erick Castillo de la Garza <ecastillo550@gmail.com>
2015-12-09 09:28:35 -0600

    fdsafdsafa

--- app-pef/Controller/Index.php
+++ app-pef/Controller/Index.php
@@ -8,8 +8,26 @@ class Index extends AbstractController{
 	}
 
 	function index() {
-		$this->redirect('/User');
-		die();
+		if ($this->auth->isAuth()) {
+			$this->user = new \Hagane\Model\User($this->auth, $this->db);
+			if ($this->user->getActivo() == 'y') {
+				if ($this->user->getUserType() == 'Administrador') {
+					$this->redirect('Admin/index');
+					die();
+				}
+				if ($this->user->getUserType() == 'Cliente') {
+					$this->redirect('Cliente/index');
+					die();
+				}
+			} else {
+				$this->auth->logout();
+				$this->redirect('User/inactivo');
+				die();
+			}
+		} else {
+			$this->redirect('/User');
+			die();
+		}
 	}
 
 	function contacto() {
