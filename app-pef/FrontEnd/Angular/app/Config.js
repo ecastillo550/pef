3aefa10b2ccd35fee1b5fe8ea7cf1495cc6ed582
Erick Castillo de la Garza <ecastillo550@gmail.com>
2015-12-09 04:01:24 -0600

    debugging de themes

--- app-pef/FrontEnd/Angular/app/Config.js
+++ app-pef/FrontEnd/Angular/app/Config.js
@@ -10,22 +10,35 @@ $id = $this->auth->isAuth();
 if (isset($id) && $id != '') {
 	$this->userManager = new \Hagane\Model\UserManagement($this->auth, $this->db);
 	$clientArray = $this->userManager->getClientUser($id);
+
+	if (isset($clientArray[0]['primary_color']) && $clientArray[0]['primary_color'] != '' && isset($clientArray[0]['secondary_color']) && $clientArray[0]['secondary_color'] != '') {
+		echo 'var userPallete = $mdThemingProvider.extendPalette(\'red\', {
+				\'500\': \''.$clientArray[0]['primary_color'].'\'
+			});';
+		echo 'var userAccentPallete = $mdThemingProvider.extendPalette("pink", {
+				"A200": "'.$clientArray[0]['secondary_color'].'"
+			});';
+
+		echo '$mdThemingProvider.definePalette("userPallete", userPallete);
+			$mdThemingProvider.definePalette("userAccentPallete", userAccentPallete);
+
+			$mdThemingProvider.theme("default")
+				.primaryPalette("userPallete")
+				.accentPalette("userAccentPallete");';
+	}
 }
 ?>
 
-var userPallete = $mdThemingProvider.extendPalette('red', {
-	'500': '<?=$clientArray[0]['primary_color']?>'
-});
-var userAccentPallete = $mdThemingProvider.extendPalette('pink', {
-	'A200': '<?=$clientArray[0]['secondary_color']?>'
-});
-$mdThemingProvider.definePalette('userPallete', userPallete);
-$mdThemingProvider.definePalette('userAccentPallete', userAccentPallete);
+// var userPallete = $mdThemingProvider.extendPalette('red', {
+// 	'500': '<?=$clientArray[0]['primary_color']?>'
+// });
+// var userAccentPallete = $mdThemingProvider.extendPalette('pink', {
+// 	'A200': '<?=$clientArray[0]['secondary_color']?>'
+// });
+// $mdThemingProvider.definePalette('userPallete', userPallete);
+// $mdThemingProvider.definePalette('userAccentPallete', userAccentPallete);
 
-$mdThemingProvider.theme('default')
-	.primaryPalette('userPallete')
-	.accentPalette('userAccentPallete');
-
-
-
+// $mdThemingProvider.theme('default')
+// 	.primaryPalette('userPallete')
+// 	.accentPalette('userAccentPallete');
 });
